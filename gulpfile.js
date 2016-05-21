8c07fb83c3e20ea40f25308625ddd96fcc776186
Ivan Villa <ivan@transmute.co>
2016-05-20 23:16:43 -0700

    Updated Build Tool
    Gulp now has broser sync, concatination, and prefixer.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
 
 
 
@@ -7,21 +10,30 @@ var sass = require('gulp-sass');
 gulp.task('sass', function(){
   return gulp.src('./src/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./build/css'));
+    .pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+    .pipe(gulp.dest('./build/css'))
+    .pipe(browserSync.stream());
 });
 
-//* Coppies The HTML file to the Build Folder
-gulp.task('html', function(){
-  gulp.src('./src/index.html')
-  .pipe(gulp.dest('./build'));
+//* Concatinate JS Files
+gulp.task('js', function(){
+  return gulp.src(['./src/js/vue.min.js','./src/js/app.js'])
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest('./build/js'))
+    .pipe(browserSync.stream());
 });
-
-
-//* Default Task
-gulp.task('default', ['sass', 'html']);
 
 
 //* Gulp watch Files
-gulp.task('watch', function() {
+gulp.task('serve', ['sass'],  function() {
+  browserSync.init({
+      server: {
+          baseDir: "./"
+      }
+  });
 gulp.watch('./src/scss/**/*', ['sass']);
+gulp.watch("*.html").on('change', browserSync.reload);
 });
